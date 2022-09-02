2dee5d93a351bfe1418bc393f38e8d7b246e9221
shyoon63 <shyoon63@e-mirim.hs.kr>
2022-09-02 13:50:42 +0900

    models Notice

--- notice/models.py
+++ notice/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Notice(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField(blank=True)  #빈칸 허용
+    create_at = models.DateTimeField(auto_now_add=True) #데이터 생성 시 날짜 시간 기록
+    modify_at = models.DateTimeField(auto_now=True) #데이터 수정 시 날짜 시간 기록
+
+    class Meta:
+        ordering = ('-modify_at', '-create_at', 'title') #수정시간 내림, 생성시간 내림, 제목 오름
+
+    def __str__(self):
+        return f'{self.title} | {self.content}'
